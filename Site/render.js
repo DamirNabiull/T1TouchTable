83e107cd31143cfe7095126dcfb389b3fe5ea809
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2022-06-08 23:12:21 +0300

    Added switch on for indicators

--- Site/render.js
+++ Site/render.js
@@ -3,16 +3,23 @@ const allCategoryButtons = document.getElementsByClassName("categoryButton");
 // MENU
 const menu = document.getElementsByClassName("menu")[0];
 const indicatorsLabel = document.getElementsByClassName("indicatorsLabel")[0];
+const indicators = document.getElementsByClassName("indicator");
 
 var choosenCategories = 0;
 
 function categoryClick(button) {
     let tempColor = window.getComputedStyle(button).backgroundColor;
     if (button.value == "on") {
+        if (choosenCategories == 1) {
+            indicatorsLabel.style.display = "none";
+        }
         choosenCategories -= 1;
         button.value = "off";
     }
     else {
+        if (choosenCategories == 0) {
+            indicatorsLabel.style.display = "inline";
+        }
         choosenCategories += 1;
         button.value = "on";
     }
